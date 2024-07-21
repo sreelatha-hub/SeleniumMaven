33ff46541bcfa4a3355fa9408877bc7d4309aed0
Lakshminarayana <Lakshminarayana@LMSREE>
2024-07-21 23:25:55 +0530

    Pushing first new code by sreelatha

---
+++ src/main/java/Selenium_Maven/Selenium_Maven/Login.java
@@ -0,0 +1,14 @@
+package Selenium_Maven.Selenium_Maven;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+	public static void main(String args[])
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\latha\\Softwares\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.geeksforgeeks.org/");
+		System.out.println(driver.getTitle());
+	}
+
+}
